1edc34f4679c113e71dcf93d8c0c28ab29144b13
Doğanay Balaban <balabandoganay@gmail.com>
2025-05-19 22:13:52 +0300

    hotfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen overflow-hidden">
+      <body className="flex flex-col min-h-screen ">
         <Navbar />
         <main className="flex-grow">
           {children}
